87731e9792c5c76f117a3aa9fdb3190fcd94105f
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-12-30 14:52:59 +0200

    codecov added

--- test/users.test.ts
+++ test/users.test.ts
@@ -34,11 +34,13 @@ describe("GET /api/users", () => {
       .set("Accept", "application/json")
       .expect("Content-Type", new RegExp("application\/json;.*"))
       .expect(200)
-      .expect((res) => {
+      .then((res) => {
         assert.equal(res.body.length, 10);
         assert.equal(Object.keys(res.body[1]).length, 4);
-      })
-      .end(done);
+        done();
+      }).catch((err) => {
+        done.fail(err);
+      });
   });
 });
 
@@ -49,17 +51,101 @@ describe("GET /api/users/{i}", () => {
       .set("Accept", "application/json")
       .expect("Content-Type", new RegExp("application\/json;.*"))
       .expect(200)
-      .expect((res) => {
+      .then((res) => {
         assert.equal(res.body.id, 1);
         assert.equal(res.body.firstName, "Tiphanie");
         assert.equal(res.body.lastName, "Upcott");
         assert.equal(res.body.age, 54);
-      })
-      .end(done);
+        done();
+      }).catch((err) => {
+        done.fail(err);
+      });
   });
   it("respond with not found when invalid id", (done) => {
     request(app)
       .get("/api/users/100")
+      .set("Accept", "application/json")
+      .expect("Content-Type", new RegExp("application\/json;.*"))
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe("POST /api/users", () => {
+  it("create a new user and respond with it", (done) => {
+    request(app)
+      .post("/api/users")
+      .send({
+        firstName: "Test",
+        lastName: "Test",
+        age: 10,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", new RegExp("application\/json;.*"))
+      .expect(201)
+      .then((res) => {
+        assert.equal(Object.keys(res.body).length, 4);
+        assert.equal(res.body.id, "11");
+        assert.equal(res.body.firstName, "Test");
+        assert.equal(res.body.lastName, "Test");
+        assert.equal(res.body.age, 10);
+        done();
+      }).catch((err) => {
+        done.fail(err);
+      });
+  });
+});
+
+describe("PUT /api/users/{i}", () => {
+  it("update user and respond with it", (done) => {
+    request(app)
+      .put("/api/users/11")
+      .send({
+        firstName: "Tester",
+        lastName: "Tester",
+        age: 20,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", new RegExp("application\/json;.*"))
+      .expect(200)
+      .then((res) => {
+        assert.equal(Object.keys(res.body).length, 4);
+        assert.equal(res.body.id, "11");
+        assert.equal(res.body.firstName, "Tester");
+        assert.equal(res.body.lastName, "Tester");
+        assert.equal(res.body.age, 20);
+        done();
+      }).catch((err) => {
+        done.fail(err);
+      });
+  });
+  it("respond with not found and not update", (done) => {
+    request(app)
+      .put("/api/users/100")
+      .send({
+        firstName: "Tester",
+        lastName: "Tester",
+        age: 20,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", new RegExp("application\/json;.*"))
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe("DELETE /api/users/{i}", () => {
+  it("delete user and respond accepted", (done) => {
+    request(app)
+      .delete("/api/users/11")
+      .set("Accept", "application/json")
+      .expect("Content-Type", new RegExp("application\/json;.*"))
+      .expect(202)
+      .end(done);
+  });
+  it("respond not found when invalid id", (done) => {
+    request(app)
+      .delete("/api/users/100")
       .set("Accept", "application/json")
       .expect("Content-Type", new RegExp("application\/json;.*"))
       .expect(404)
